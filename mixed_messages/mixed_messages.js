a004a740af2fb2a6df569c369f631bce18420d19
orange-julian <juliannagy@Julians-MacBook-Air.local>
2023-04-16 21:48:18 -0400

    adding new version

--- mixed_messages/mixed_messages.js
+++ mixed_messages/mixed_messages.js
@@ -37,21 +37,38 @@ Topic:
 
 */
 
-function randNum (range) {
-    return Math.floor(Math.random()*range);
+// // Weather predictor version with generated content
+// function randNum (range) {
+//     return Math.floor(Math.random()*range);
+// }
+// // console.log(randNum(10));
+
+// function randMessage () {
+//     // choose a random date within the next 10 years and make string as the first 
+//     let randDate = (new Date(2023 + randNum(10),randNum(12),randNum(31)+1)).toDateString();
+//     // create a list of weather conditions and choose at random.
+//     let weather = ['sunny', 'cloudy', 'rainy', 'stormy', 'partly cloudy', 'foggy', 'windy'];
+//     const randWeather = weather[randNum(7)];
+//     // choose a random temperature between 0 and 30.
+//     let randTemp = randNum(31);
+//     // string random selections together into a sentence.
+//     console.log(`On ${randDate}, the weather will be ${randWeather} and the temperature will be ${randTemp}ºC.`);
+// }
+
+// randMessage();
+
+// Weather predictor version with stored content 
+function randNum () {
+    return Math.floor(Math.random()*7);
 }
-// console.log(randNum(10));
+// create set of arrays for pulling
+const weatherGroups = {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    condition: ['sunny', 'cloudy', 'rainy', 'stormy', 'partly cloudy', 'foggy', 'windy'],
+    mood: ['happy', 'sad', 'grateful', 'nostalgic', 'sleepy', 'lonely', 'joyful']
+}
 
 function randMessage () {
-    // choose a random date within the next 10 years and make string as the first 
-    let randDate = (new Date(2023 + randNum(10),randNum(12),randNum(31)+1)).toDateString();
-    // create a list of weather conditions and choose at random.
-    let weather = ['sunny', 'cloudy', 'rainy', 'stormy', 'partly cloudy', 'foggy', 'windy'];
-    const randWeather = weather[randNum(7)];
-    // choose a random temperature between 0 and 30.
-    let randTemp = randNum(31);
-    // string random selections together into a sentence.
-    return `On ${randDate}, the weather will be ${randWeather} and the temperature will be ${randTemp}ºC.`;
+    console.log(`On ${weatherGroups.days[randNum()]}, the weather will be ${weatherGroups.condition[randNum()]} and you will be feeling ${weatherGroups.mood[randNum()]}.`)
 }
-
-console.log(randMessage());
+randMessage();
